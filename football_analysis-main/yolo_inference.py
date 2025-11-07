c8ffcb46c300c9b37c35b79e83c117b2bd427ffa
codebookreader <patrick.muiruri@strathmore.edu>
2025-11-07 10:08:26 +0300

    Platform dashboards created

--- football_analysis-main/yolo_inference.py
+++ football_analysis-main/yolo_inference.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8x')  # load a pretrained model (recommended for training)
 
-results = model.predict(r'C:\Users\USER\.vscode\ScoutKenya\football_analysis-main\input_videos\Untitled video 10 - Made with Clipchamp.mp4', save=True)
+results = model.predict(r'C:\Users\USER\.vscode\ScoutKenya\football_analysis-main\input_videos\Untitled video 13 -2 - Made with Clipchamp.mp4', save=True)
 print(results[0])
 print('=====================================')
 for box in results[0].boxes:
